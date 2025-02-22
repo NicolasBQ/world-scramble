a3d759540612221cfb4fe04a049ff0e0862f3276
Nicolas Baracaldo <nicolasbquintero@gmail.com>
2025-02-22 10:53:46 -0500

    Change Input

--- src/UserInput.tsx
+++ src/UserInput.tsx
@@ -1,8 +1,20 @@
+import React, { useRef } from "react"
+
 interface userInputProps {
     word: string
 }
 
 const UserInput = ({ word }: userInputProps) => {
+    const inputRef = useRef<Array<HTMLInputElement | null>>([]);
+
+    const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>,index: number) => {
+        if(event.key === 'Backspace' && index >= 0) {
+            inputRef.current[index - 1]?.focus();
+        } else if(index < word.length - 1) {
+            inputRef.current[index + 1]?.focus();
+        }
+    }
+
     return (
         <form className="mt-8 flex justify-center gap-1 md:gap-2 flex-wrap">
             {
@@ -11,6 +23,9 @@ const UserInput = ({ word }: userInputProps) => {
                         type="text" 
                         className="custom-input"
                         key={i}
+                        ref={el => inputRef.current[i] = el}
+                        maxLength={1}
+                        onKeyUp={(e) => handleKeyUp(e, i)}
                     /> 
                 ))
             }   
